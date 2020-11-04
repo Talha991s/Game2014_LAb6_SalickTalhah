8528fc6bd7c6d3fac249826afb4bc801456aba55
Talha991s <55731371+Talha991s@users.noreply.github.com>
2020-11-04 02:42:39 -0800

    Added Jump and AutoCam.

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Profiling;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
     public Joystick joystick;
-    public float joystickSensitivity;
+    public float joystickSensitivityHorizontal;
+    public float joystickSensitivityVertical;
     public float horizontalspeed;
     public float jumpForce;
     public float maximumVelocity;
-    public Rigidbody2D rigidbody2D;
+    public bool isGrounded;
+
+    private Rigidbody2D rigidbody2D;
+
+    private SpriteRenderer m_spriterenderer;
+    private Animator m_animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        m_spriterenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,14 +34,39 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void _Move()
     {
-        if(joystick.Horizontal > joystickSensitivity)
+        if (isGrounded)
         {
-            rigidbody2D.AddForce(Vector2.right * horizontalspeed * Time.deltaTime);
-        }
+            if (joystick.Horizontal > joystickSensitivityHorizontal)
+            {
+                rigidbody2D.AddForce(Vector2.right * horizontalspeed * Time.deltaTime);
+                m_spriterenderer.flipX = false;
 
-        if (joystick.Horizontal < -joystickSensitivity)
-        {
-            rigidbody2D.AddForce(Vector2.left * horizontalspeed * Time.deltaTime);
+                m_animator.SetInteger("AnimState", 1);
+            }
+            else if (joystick.Horizontal < -joystickSensitivityHorizontal)
+            {
+                rigidbody2D.AddForce(Vector2.left * horizontalspeed * Time.deltaTime);
+                m_spriterenderer.flipX = true;
+                m_animator.SetInteger("AnimState", 1);
+            }
+            else if (joystick.Vertical > joystickSensitivityVertical)
+            {
+                rigidbody2D.AddForce(Vector2.up * jumpForce * Time.deltaTime);
+                m_animator.SetInteger("AnimState", 2);
+            }
+            else
+            {
+                m_animator.SetInteger("AnimState", 0);
+            }
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        isGrounded = false;
     }
 }
